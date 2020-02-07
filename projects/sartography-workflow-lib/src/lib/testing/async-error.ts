ee784de751e68eac3293c8768877af0788824bfb
Aaron Louie <ajlouie@gmail.com>
2020-02-07 10:10:27 -0500

    Removes unused code

--- projects/sartography-workflow-lib/src/lib/testing/async-error.ts
+++
@@ -1,7 +0,0 @@
-/** Create async observable error that errors
- *  after a JS engine turn */
-import {defer} from 'rxjs';
-
-export function asyncError<T>(errorObject: any) {
-  return defer(() => Promise.reject(errorObject));
-}
