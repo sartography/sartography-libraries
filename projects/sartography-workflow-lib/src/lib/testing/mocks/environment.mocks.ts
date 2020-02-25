2b9a1dbb7fc2eee456d36299f29fe682a30ab121
Aaron Louie <ajlouie@gmail.com>
2020-02-25 11:59:03 -0500

    Upgrades to Angular v9

--- projects/sartography-workflow-lib/src/lib/testing/mocks/environment.mocks.ts
+++ projects/sartography-workflow-lib/src/lib/testing/mocks/environment.mocks.ts
@@ -1,5 +1,7 @@
 import {AppEnvironment} from '../../types/app-environment';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class MockEnvironment implements AppEnvironment {
   production = false;
   api = 'apiRoot';
