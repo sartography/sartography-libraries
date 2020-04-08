95b61b2359b03817917273d3dcde146e6df1d9a4
Aaron Louie <ajlouie@gmail.com>
2020-04-08 16:29:29 -0400

    Uses timecode

--- projects/sartography-workflow-lib/src/lib/testing/mocks/file.mocks.ts
+++ projects/sartography-workflow-lib/src/lib/testing/mocks/file.mocks.ts
@@ -4,12 +4,15 @@ import {mockTask0} from './task.mocks';
 import {mockWorkflowSpec0} from './workflow-spec.mocks';
 import {mockWorkflow0} from './workflow.mocks';
 
-const options = {lastModified: new Date('2020-01-23T12:34:12.345Z').getTime()};
+const timeCode = new Date('2020-01-23T12:34:12.345Z').getTime();
 export const mockFileMeta0: FileMeta = {
   id: 0,
   content_type: 'text/xml',
   name: 'one-fish.bpmn',
-  file: new File([], 'one-fish.bpmn', options),
+  file: new File([], 'one-fish.bpmn', {
+    type: 'text/xml',
+    lastModified: timeCode,
+  }),
   type: FileType.BPMN,
   workflow_spec_id: mockWorkflowSpec0.id,
   task_id: mockTask0.id,
@@ -21,7 +24,10 @@ export const mockFileMeta1: FileMeta = {
   id: 1,
   content_type: 'text/xml',
   name: 'two-fish.bpmn',
-  file: new File([], 'two-fish.bpmn', options),
+  file: new File([], 'two-fish.bpmn', {
+    type: 'text/xml',
+    lastModified: timeCode,
+  }),
   type: FileType.BPMN,
   workflow_spec_id: mockWorkflowSpec0.id,
   task_id: mockTask0.id,
@@ -33,7 +39,10 @@ export const mockFileMeta2: FileMeta = {
   id: 2,
   content_type: 'text/xml',
   name: 'red-fish.bpmn',
-  file: new File([], 'red-fish.bpmn', options),
+  file: new File([], 'red-fish.bpmn', {
+    type: 'text/xml',
+    lastModified: timeCode,
+  }),
   type: FileType.BPMN,
   workflow_spec_id: mockWorkflowSpec0.id,
   task_id: mockTask0.id,
@@ -45,7 +54,10 @@ export const mockFileMeta3: FileMeta = {
   id: 3,
   content_type: 'text/xml',
   name: 'blue-fish.bpmn',
-  file: new File([], 'blue-fish.bpmn', options),
+  file: new File([], 'blue-fish.bpmn', {
+    type: 'text/xml',
+    lastModified: timeCode,
+  }),
   type: FileType.BPMN,
   workflow_spec_id: mockWorkflowSpec0.id,
   task_id: mockTask0.id,
@@ -57,7 +69,10 @@ export const mockFileMetaTask0: FileMeta = {
   id: 4,
   content_type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
   name: 'my_doc.docx',
-  file: new File([], 'my_doc.docx', options),
+  file: new File([], 'my_doc.docx', {
+    type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    lastModified: timeCode,
+  }),
   type: FileType.DOCX,
   workflow_id: mockWorkflow0.id,
   study_id: mockWorkflow0.study_id,
