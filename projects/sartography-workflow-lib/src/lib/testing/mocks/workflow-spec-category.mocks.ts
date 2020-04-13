344892c7e3b020c2299502d457619b33ab0400b4
Aaron Louie <ajlouie@gmail.com>
2020-04-13 10:24:50 -0400

    Changes category display order

--- projects/sartography-workflow-lib/src/lib/testing/mocks/workflow-spec-category.mocks.ts
+++ projects/sartography-workflow-lib/src/lib/testing/mocks/workflow-spec-category.mocks.ts
@@ -5,21 +5,21 @@ export const mockWorkflowSpecCategory0: WorkflowSpecCategory = {
   id: 0,
   name: 'all_things',
   display_name: 'Everything',
-  display_order: 2,
+  display_order: 0,
   workflows: mockWorkflowStats,
 };
 export const mockWorkflowSpecCategory1: WorkflowSpecCategory = {
   id: 1,
   name: 'few_things',
   display_name: 'Some things',
-  display_order: 0,
+  display_order: 1,
   workflows: mockWorkflowStats,
 };
 export const mockWorkflowSpecCategory2: WorkflowSpecCategory = {
   id: 2,
   name: 'one_thing',
   display_name: 'One thing',
-  display_order: 1,
+  display_order: 2,
   workflows: mockWorkflowStats,
 };
 
