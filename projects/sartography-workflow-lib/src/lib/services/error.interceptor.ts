e70c294443b715ae9adf317d9d4bc52b88afa8cc
Aaron Louie <ajlouie@gmail.com>
2020-05-23 13:16:46 -0400

    Handles redirect to login differently on production than in test/dev environment

--- projects/sartography-workflow-lib/src/lib/services/error.interceptor.ts
+++ projects/sartography-workflow-lib/src/lib/services/error.interceptor.ts
@@ -1,30 +1,45 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {Observable, throwError} from 'rxjs';
 import {catchError} from 'rxjs/operators';
+import {AppEnvironment} from '../types/app-environment';
 import {ApiService} from './api.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router, private apiService: ApiService) {
+  constructor(
+    private router: Router,
+    private apiService: ApiService,
+    @Inject('APP_ENVIRONMENT') private environment: AppEnvironment,
+  ) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
-      if (err.status === 401) {
-        // Watch for a redirect on calls to the api, not permission denied.
 
-        // auto logout if 401 response returned from api
+      // Watch for a redirect on calls to the API, not permission denied.
+      if (err.status === 401 || err.status === 403) {
+
+        // auto logout if 401 or 403 response returned from API
         localStorage.removeItem('currentUser');
-        // get the url of the page the user is currently on, and save it to the
-        // 'previous_page' in local storage.
-        localStorage.setItem('prev_url', this.router.url);
 
         // Redirect users through the login process, passing in the url back
         // to the session endpoint, so we can capture the token and save it.
-        this.apiService.redirectToLogin(this.router.url)
+        if (this.environment.production) {
+          console.log('ErrorInterceptor redirecting to API login');
+
+          // get the url of the page the user is currently on, and save it in
+          // local storage.
+          localStorage.setItem('prev_url', location.href);
+          this.apiService.redirectToLogin(location.origin + '/session');
+        } else {
+          console.log('ErrorInterceptor redirecting to /');
+
+          // We're not on production, so just redirect to the fake login screen.
+          this.apiService.openUrl('/');
+        }
       }
 
       const error = err.error.message || err.statusText;
