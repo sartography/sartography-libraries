704347315f1763af8a843cbd345c90261e17110a
Aaron Louie <ajlouie@gmail.com>
2020-07-27 16:26:28 -0400

    Cleans up formatting

--- projects/sartography-workflow-lib/src/lib/testing/mocks/workflow-nav.mocks.ts
+++ projects/sartography-workflow-lib/src/lib/testing/mocks/workflow-nav.mocks.ts
@@ -1,24 +1,19 @@
-import {Workflow, WorkflowNavItem, WorkflowStatus} from '../../types/workflow';
-import {mockStudy0} from './study.mocks';
-import {mockWorkflowSpec0, mockWorkflowSpec1} from './workflow-spec.mocks';
-import {mockWorkflowTask0, mockWorkflowTask1, mockWorkflowTasks} from './workflow-task.mocks';
+import {WorkflowNavItem} from '../../types/workflow';
 
-export const mockNav0: WorkflowNavItem[] =
-  [
-    {
-      child_count: 0, id: 0, indent: 0, level: 0, name: '', state: undefined, task_id: '', title: ''
-    },
-    {
-      child_count: 0, id: 0, indent: 0, level: 0, name: '', state: undefined, task_id: '', title: ''
-    }
-    ];
+export const mockNav0: WorkflowNavItem[] = [
+  {
+    child_count: 0, id: 0, indent: 0, level: 0, name: '', state: undefined, task_id: '', title: ''
+  },
+  {
+    child_count: 0, id: 0, indent: 0, level: 0, name: '', state: undefined, task_id: '', title: ''
+  }
+];
 
-export const mockNav1: WorkflowNavItem[] =
-  [
-    {
-      child_count: 0, id: 0, indent: 0, level: 0, name: '', state: undefined, task_id: '', title: ''
-    },
-    {
-      child_count: 0, id: 0, indent: 0, level: 0, name: '', state: undefined, task_id: '', title: ''
-    }
-  ];
+export const mockNav1: WorkflowNavItem[] = [
+  {
+    child_count: 0, id: 0, indent: 0, level: 0, name: '', state: undefined, task_id: '', title: ''
+  },
+  {
+    child_count: 0, id: 0, indent: 0, level: 0, name: '', state: undefined, task_id: '', title: ''
+  }
+];
