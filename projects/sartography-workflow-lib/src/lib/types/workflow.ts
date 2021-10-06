fc2378c5addf86ca8b0abc6112e7648fa0a9750a
alicia pritchett <aliciateaford@gmail.com>
2021-10-06 12:55:01 -0400

    Category ID optional (for new categories)

--- projects/sartography-workflow-lib/src/lib/types/workflow.ts
+++ projects/sartography-workflow-lib/src/lib/types/workflow.ts
@@ -36,7 +36,7 @@ export interface WorkflowSpec {
 }
 
 export interface WorkflowSpecCategory {
-  id: number;
+  id?: number;
   display_name: string;
   display_order: number;
   workflows?: WorkflowMetadata[];
