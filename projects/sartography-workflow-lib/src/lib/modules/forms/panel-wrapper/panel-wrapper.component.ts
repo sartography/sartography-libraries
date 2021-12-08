82f51b4f747728e15c56068693d9d2d2f5ca9c7d
alicia pritchett <aliciateaford@gmail.com>
2021-12-08 13:17:58 -0500

    This thing might actually work now
    Remove hide_expression checks from within the group repeat wrapper. Why? Because groups should hide groups, and fields should hide fields. And Repeats are just composites of groups and fields

--- projects/sartography-workflow-lib/src/lib/modules/forms/panel-wrapper/panel-wrapper.component.ts
+++ projects/sartography-workflow-lib/src/lib/modules/forms/panel-wrapper/panel-wrapper.component.ts
@@ -10,38 +10,32 @@ export class PanelWrapperComponent extends FieldWrapper {
 
   // Loop through every field in group. If all are hidden, hide the group wrapper
    shouldHide(): boolean {
-     // Are we in a repeat? If so:
-     if (this.field.type == 'repeat') {
+     // Are we in a repeat? If so, evaluate the hide_expression applied to the repeat group:
+     if (this.field.type == 'repeat' && this.field.fieldArray.fieldGroup) {
        for (let x = 0; x < this.field.fieldArray.fieldGroup.length; x++) {
-      if (typeof (this.field.fieldArray.fieldGroup[0].hideExpression) === 'function') {
-        if ((this.field.fieldArray.fieldGroup[0].hideExpression(this.field.parent && this.field.parent.model, this.formState, this.field)) !== true) {
-          console.log('weird thing - show group');
-          return false;
+          if (typeof (this.field.fieldArray.fieldGroup[0].hideExpression) === 'function') {
+            if ((this.field.fieldArray.fieldGroup[0].hideExpression(this.field.parent && this.field.parent.model, this.formState, this.field)) !== true) {
+            return false;
+          }
         }
       }
-    }
     // If everything in the group is hidden, hide the entire group.
-    console.log('Hiding Repeat');
      return true;
      }
 
-     // If nothing is in the fieldGroup (i.e. a new repeat section), don't hide the group.
-     if (this.field.fieldGroup.length == 0) {
-       console.log('group is empty; show it');
+     // If nothing is in the fieldGroup, don't automatically hide the group.
+     if (this.field.fieldGroup && this.field.fieldGroup.length == 0) {
        return false;
      }
      // If anything in the field group is visible, don't hide the group.
      if (this.field.fieldGroup) {
        for (let x = 0; x < this.field.fieldGroup.length; x++) {
          if (this.field.fieldGroup[x].hide !== true ) {
-           console.log('somethin is visible; show it');
-           console.log('FG',this.field.fieldGroup);
            return false;
          }
        }
      }
      // If everything in the group is hidden, hide the entire group.
-     console.log('hide the group');
      return true;
    }
 }
