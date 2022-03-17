67286ca2216bd70b2fcc60d3a4069ef9db0e8937
alicia pritchett <aliciateaford@gmail.com>
2022-03-17 10:33:27 -0400

    put a null meta object on mock cats

--- projects/sartography-workflow-lib/src/lib/testing/mocks/workflow-spec-category.mocks.ts
+++ projects/sartography-workflow-lib/src/lib/testing/mocks/workflow-spec-category.mocks.ts
@@ -6,12 +6,19 @@ export const mockCategoryMetaData: WorkflowCategoryMetadata = {
   state: WorkflowState.HIDDEN,
 }
 
+export const mockCategoryMetaData2: WorkflowCategoryMetadata = {
+  id: null,
+  state: null,
+  state_message: null,
+}
+
 export const mockWorkflowSpecCategory0: WorkflowSpecCategory = {
   id: '0',
   display_name: 'Everything',
   display_order: 0,
   workflows: mockWorkflowMetas,
   admin: false,
+  meta: mockCategoryMetaData2
 };
 export const mockWorkflowSpecCategory1: WorkflowSpecCategory = {
   id: '1',
@@ -19,6 +26,7 @@ export const mockWorkflowSpecCategory1: WorkflowSpecCategory = {
   display_order: 1,
   workflows: mockWorkflowMetas,
   admin: false,
+  meta: mockCategoryMetaData2
 };
 export const mockWorkflowSpecCategory2: WorkflowSpecCategory = {
   id: '2',
@@ -26,6 +34,7 @@ export const mockWorkflowSpecCategory2: WorkflowSpecCategory = {
   display_order: 2,
   workflows: mockWorkflowMetas,
   admin: false,
+  meta: mockCategoryMetaData2
 };
 export const mockWorkflowSpecCategory3: WorkflowSpecCategory = {
   id: '3',
@@ -33,6 +42,7 @@ export const mockWorkflowSpecCategory3: WorkflowSpecCategory = {
   display_order: 3,
   workflows: mockWorkflowMetas,
   admin: false,
+  meta: mockCategoryMetaData,
 };
 
 export const mockWorkflowSpecCategories: WorkflowSpecCategory[] = [
