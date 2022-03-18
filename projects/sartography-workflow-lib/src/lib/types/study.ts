4ccce01b819ab61fb4174e3775eebeb5c1384d27
alicia pritchett <aliciateaford@gmail.com>
2022-03-18 12:05:19 -0400

    remove references to primary investigator id

--- projects/sartography-workflow-lib/src/lib/types/study.ts
+++ projects/sartography-workflow-lib/src/lib/types/study.ts
@@ -101,7 +101,6 @@ export interface Study {
   inactive?: boolean;
   ind_number?: string;
   last_updated?: Date;
-  primary_investigator_id?: string;
   primary_investigator?: string;
   status?: StudyStatus;
   sponsor?: string;
