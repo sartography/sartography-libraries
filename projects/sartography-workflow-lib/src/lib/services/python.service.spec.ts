6017cab8b63123d44a23e0dcf97d2b65ad9249ec
Dan <daniel.h.funk@gmail.com>
2022-04-12 08:30:05 -0400

    Removing all backend calls for python execution that are related to hide expressions, default values, labels, and other expressions set in Formly.  These methods get called 1000's of times on some pages. All new working code is in the python.service with good tests in the python.service.spec. Also included here, but not used, is the piodide service which can execute python using web assembly, but proved to be too slow for hide expressions, but which could be very useful for our BPMN editor.  Also included, but not currently in use is the script service, which can be used to load large librarires (like piodide) over a CDN to make loading far more efficient.

---
+++ projects/sartography-workflow-lib/src/lib/services/python.service.spec.ts
@@ -0,0 +1,147 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PythonService } from './python.service';
+
+describe('PythonService', () => {
+  let service: PythonService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PythonService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should evaluate double quoted string expressions', () => {
+    expect(service.eval('"anything"', {})).toEqual('anything')
+  })
+
+  it('should evaluate single quoted string expressions', () => {
+    expect(service.eval('\'anything\'', {})).toEqual('anything')
+  })
+
+  it('should evaluate True / False', () => {
+    expect(service.eval('True', {})).toEqual(true)
+    expect(service.eval('False', {})).toEqual(false)
+  })
+
+  it('should evaluate numbers', () => {
+    expect(service.eval('1', {})).toEqual(1)
+  })
+
+  it('should evaluate variables', () => {
+    expect(service.eval('my_var', {'my_var': 'yo!'})).toEqual('yo!')
+  })
+
+  it('should handle the not opperator', () => {
+    expect(service.eval('not True', {})).toEqual(false)
+    expect(service.eval('not(True)', {})).toEqual(false)
+    expect(service.eval('not(var)', {"var":true})).toEqual(false)
+    expect(service.eval('not(var)', {"var":false})).toEqual(true)
+  })
+
+  it ('should disregard parens if completely surrounding an expression', () => {
+    expect(service.eval('("any old thing")', {})).toEqual("any old thing")
+  })
+
+
+  it ('should handle dot notation', () => {
+    let ctx = {"a": {"b": { "c": true }}}
+    expect(service.eval('a.b.c', ctx)).toEqual(true)
+    expect(service.eval('not a.b.c', ctx)).toEqual(false)
+  })
+
+  it ('should handle comparisons', () => {
+    expect(service.eval('1 == 2', {})).toEqual(false)
+    expect(service.eval('1 != 2', {})).toEqual(true)
+
+    let ctx = {"a": {"b": { "c": "Wilma", "d": "Fred"}}}
+    expect(service.eval('a.b.c == "Fred"', ctx)).toEqual(false)
+    expect(service.eval('a.b.c == "Wilma"', ctx)).toEqual(true)
+    expect(service.eval('a.b.c != a.b.d', ctx)).toEqual(true)
+  })
+
+  it ('should handle length expressions', () => {
+    expect(service.eval('len("apple")', {})).toEqual(5)
+    expect(service.eval('len(var)', {var:"apple"})).toEqual(5)
+  })
+
+  it ('should handle the "in" or includes operator', () => {
+    expect(service.eval('"a" in ["a","b"]', {})).toEqual(true)
+    expect(service.eval('"a" in ["b","c"]', {})).toEqual(false)
+  })
+
+  /*
+  CollStorUVaLocPaperTypes is None or "OtherTypes" not in CollStorUVaLocPaperTypes
+   */
+  it ('should handle the "not in" or includes operator', () => {
+    expect(service.eval('"a" not in ["a","b"]', {})).toEqual(false)
+    expect(service.eval('"a" not in ["b","c"]', {})).toEqual(true)
+  })
+
+/*
+  <camunda:property id="hide_expression" value="not AsApplConsentDocType or consent_docs[AsApplConsentDocType].has_description  != &#34;√&#34;" />
+  */
+  it ('should handle bracket notation.', () => {
+    let ctx = {"alpha":{"beta":{"gamma": "apple"}}, "var": "beta"}
+    expect(service.eval('alpha["beta"]["gamma"]', ctx)).toEqual("apple")
+    expect(service.eval('alpha[var]["gamma"]', ctx)).toEqual("apple")
+  })
+
+  /*  We need to support this use case I suspect.
+  it ('should handle get expressions within bracket notation.', () => {
+    let ctx = {"alpha":{"beta":{"gamma": "apple"}}, "var": "beta"}
+    expect(service.eval('alpha.get("beta").gamma', ctx)).toEqual("apple")
+  })
+   */
+
+  /*
+  <camunda:property id="hide_expression" value="CollStorUVaLocPaperTypes is None or &#34;OtherTypes&#34; not in CollStorUVaLocPaperTypes" />
+ */
+  it ('should handle is none.', () => {
+    let ctx = {"my_check": null}
+    expect(service.eval('my_check is None', ctx)).toEqual(true)
+  });
+
+/*
+    <camunda:property id="hide_expression" value="isIVRSIWRSIXRSMan or RequiredIVRS_IWRS_IXRS == &#34;no&#34; or RequiredIVRS_IWRS_IXRS is None" />
+ */
+  it ('should handle multiple or statements.', () => {
+    let ctx = {"a": null, "b": null, "c": 2}
+    expect(service.eval('a or b is not None or c == 2', ctx)).toEqual(true)
+  })
+
+  /*
+  "" if not AsApplAncillaryDocType else ancillary_docs[AsApplAncillaryDocType]["doczip"]
+  */
+  it ('should handle the if else expression', () => {
+    expect(service.eval('"x" if True else "y"', {})).toEqual(('x'))
+    expect(service.eval('"x" if False else "y"', {})).toEqual(('y'))
+    expect(service.eval('a if b else c', {"a":"a", "b": true, c:"c"})).toEqual(('a'))
+  })
+
+  it ('should handle some complex groupings of expressions', () => {
+    //  ex.  not var or var in ["a","b"]
+    expect(service.eval('x or var == "c"', {"x": true, "var": "a"})).toEqual(true)
+    expect(service.eval('x or var == "c"', {"x": false, "var": "c"})).toEqual(true)
+    expect(service.eval('x or var == "c"', {"x": false, "var": "a"})).toEqual(false)
+
+    expect(service.eval('not var or var in ["a","b"]', {"var": "a"})).toEqual(true)
+    expect(service.eval('not var or var in ["1","2"]', {"var": "c"})).toEqual(false)
+    expect(service.eval('not var or var in ["Z","X"]', {"var": null})).toEqual(true)
+
+    expect(service.eval('not var or var["var2"] != "b"', {"var": {"var2": "a"}})).toEqual(true)
+    expect(service.eval('not var or var["var2"] != "a"', {"var": {"var2": "a"}})).toEqual(false)
+
+  })
+
+  it ('should handle startswith method', () => {
+    expect(service.eval('"john".startswith("j")', {})).toEqual(true)
+    expect(service.eval('"john".startswith("Q")', {})).toEqual(false)
+    expect(service.eval('a.b.startswith("c")', {a: {b: "cat"}})).toEqual(true)
+    expect(service.eval('a.b.startswith(c)', {a: {b: "cat"}, c:"c"})).toEqual(true)
+  })
+
+});
---
+++ projects/sartography-workflow-lib/src/lib/services/python.service.ts
@@ -0,0 +1,188 @@
+import { Injectable } from '@angular/core';
+import {ScriptService} from './script.service';
+import { cloneDeep } from 'lodash';
+import {defer, from, Observable, of} from 'rxjs';
+import {not} from 'rxjs/internal-compatibility';
+
+/**
+ * Does a very mediocre effort at evaluating short oneline python expressions in Javascript.
+ * I did this using Regular Expressions - which is a terrible way to go about this.  Would be far better to use a lexer.
+ * There are good tests, so this is really here to be re-written by a better person than I.
+ */
+@Injectable({providedIn: 'root'})
+export class PythonService {
+
+  constructor() {
+  }
+
+  isReady() {
+    // We are always ready baby. (here because we may want to switch out to the piodide service someday, which is
+    // definitely not "always ready"
+    return true
+  }
+
+  public ready(): Observable<any> {
+    return of(null)  // We are always ready.
+  }
+
+
+  /**
+   * I am ashamed of this method. It would be far better to tokenize and process.
+   * @param expression
+   * @param context
+   * @param defaultResult
+   */
+  eval(expression, context, defaultResult = "") {
+    expression = expression.trim()
+
+    // If this is True or False, just return that.
+    if (expression === 'True') return true;
+    if (expression === 'False') return false;
+    if (expression === 'None') return null;
+
+    // If this is just a double-quoted string, evaluate it to handle any escaped quotes.
+    let match = expression.match(/^"(?:[^"\\]|\\.)*"$/)
+    if (match) {
+      return eval(expression);
+    }
+
+    // If this is just a single-quoted string, evaluate it to handle any escaped quotes.
+    let quote_match = expression.match(/^'(?:[^'\\]|\\.)*'$/)
+    if (quote_match) {
+      return eval(expression);
+    }
+
+    // If this is just numbers, eval it.
+    let number_match = expression.match(/^\d+$/is)
+    if (number_match) {
+      return eval(expression);
+    }
+
+    // If this is a literal array, eval it.  ie ['a','b']
+    let array_match = expression.match(/^\[.*\]$/)
+    if (array_match) {
+      return eval(expression)
+    }
+
+    // If this is surrounded by parens, disregard them.
+    let paren_match = expression.match(/^\((.*)\)$/)
+    if (paren_match) {
+      return this.eval(paren_match[1], context);
+    }
+
+    // If this is a single world (no spaces).
+    // Also, handle any dot notation in the process.
+    if (expression.match(/^[\w_\-.]+$/)) {
+      return expression.split('.').reduce((o, i) => o[i], context)
+    }
+
+    // If this is an expression with bracket syntax, evaluate it.
+    // ie var1['property']  or v1['prop1']['subprop'] or even
+    // var1[var2]['subprop']
+    let braket_match = expression.match(/^(\w+)(\[[\w\'\"-_]+\])+$/)
+    if (braket_match) {
+      let my_value = context[braket_match[1]]
+      for (let match of expression.matchAll(/\[(['"]?)([\w-_]+)['"]?\]+/g)){
+        if(match[1]) {  // for x['y']  where 'y' is a string.
+          my_value = my_value[match[2]]
+        } else {        // for x[y] where y is a variable.
+          my_value = my_value[this.eval(match[2], context)]
+        }
+      }
+      return my_value
+    }
+
+    // if this is a start's with expression
+    let starts_with_match = expression.match(/^([\"\w\.]+)\.startswith\(((["']?)[^)]+\3)\)$/)
+    if (starts_with_match) {
+      let base = this.eval(starts_with_match[1], context)
+      let start = this.eval(starts_with_match[2], context)
+      return base.startsWith(start)
+    }
+
+    // If this is a get expression. ie dict.get('abc')
+/*
+    let get_match = expression.match(/^([\"\w\.]+)\.get\(((["']?)[^)]+\3)\)$/)
+    if (get_match) {
+      let base = this.eval(get_match[1], context)
+      let start = this.eval(get_match[2], context)
+      return base.get(start)
+    }
+*/
+
+    // If this is an expression that matches not XXX or not(XXX), where XXX is in the model, eval that.
+    let not_match = expression.match(/^not[ \(]([\w\.]+)\)?$/)
+    if (not_match) {
+      let base = this.eval(not_match[1], context)
+      if (base == null) {
+        return true
+      } else {
+        return !(base)
+      }
+    }
+
+    let len_match = expression.match(/^len\((.+)\)$/)
+    if (len_match) {
+      return this.eval(len_match[1], context).length
+    }
+
+    let if_else_match = expression.match(/^(.*) if (.*) else (.*)$/)
+    if (if_else_match) {
+      let condition = this.eval(if_else_match[2], context)
+      if(condition) {
+        return this.eval(if_else_match[1], context)
+      } else {
+        return this.eval(if_else_match[3], context)
+      }
+    }
+
+    let or_match = expression.match(/(.*) (or) ?(.*)$/)
+    if (or_match) {
+      let arg1 = this.eval(or_match[1], context)
+      if (arg1) {
+        return arg1
+      } else {
+        return this.eval(or_match[3], context)
+      }
+    }
+
+
+    // If this contains a comparison, split, eval each side, and compare the two.
+    // Perfer to split on comparisons first.
+    let compare_match = expression.match(/(.*) ?(==|!=| is ) ?(.*)$/)
+    if (compare_match) {
+      let arg1 = this.eval(compare_match[1], context)
+      let arg2 = this.eval(compare_match[3], context)
+      let comp = compare_match[2]
+      if (comp == '!=')
+        return arg1 !== arg2
+      else
+        return arg1 === arg2
+    }
+
+    let not_in_match = expression.match(/(.*) ?( not in ) ?(.*)$/)
+    if (not_in_match) {
+      let arg1 = this.eval(not_in_match[1], context)
+      let arg2 = this.eval(not_in_match[3], context)
+      return !arg2.includes(arg1)
+    }
+
+    let and_in_match = expression.match(/(.*) ?( and | in ) ?(.*)$/)
+    if (and_in_match) {
+      let arg1 = this.eval(and_in_match[1], context)
+      let arg2 = this.eval(and_in_match[3], context)
+      let comp = and_in_match[2]
+      if (comp == ' and ')
+        return arg1 && arg2
+      else if (comp == ' in ')
+        return arg2.includes(arg1)
+    }
+
+    if (defaultResult === "no_default") {
+      throw SyntaxError("unable to evaluate expression " + expression)
+    } else {
+      console.error("Failed to parse expression ", expression)
+      return defaultResult
+    }
+  }
+}1
